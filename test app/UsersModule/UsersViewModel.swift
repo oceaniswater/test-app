53073d53e882b1d087f6913925db41aaff060029
oceaniswater <markflyingbear@gmail.com>
2023-07-15 11:48:45 +0100

    add fetch func with async await

--- test app/UsersModule/UsersViewModel.swift
+++ test app/UsersModule/UsersViewModel.swift
@@ -67,7 +67,7 @@ final class UsersViewModel: ObservableObject {
                 .receive(on: DispatchQueue.main)
                 .tryMap({ result in
                     guard let response = result.response as? HTTPURLResponse,
-                          response.statusCode >= 200 && response.statusCode <= 300 else {
+                          response.statusCode >= 200 && response.statusCode < 300 else {
                         throw UserError.invalidStatusCode
                     }
                     
@@ -94,6 +94,33 @@ final class UsersViewModel: ObservableObject {
                 .store(in: &bag)
         }
         
+    }
+    
+    @MainActor
+    func fetchUsersAsyncAwait() async throws {
+        let urlString = "https://jsonplaceholder.typicode.com/users"
+        if let url = URL(string: urlString) {
+            isRefreshing = true
+            defer { isRefreshing = false }
+            
+            do {
+                let (data, response) = try await URLSession.shared.data(from: url)
+                
+                guard let response = response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw UserError.invalidStatusCode
+                }
+                
+                let decoder = JSONDecoder()
+                guard let users = try? decoder.decode([User].self, from: data) else {
+                    throw UserError.failedToDecode
+                }
+                
+                self.users = users
+            } catch {
+                throw UserError.custom(error: error)
+            }
+        }
     }
 }
 
--- test app/UsersModule/View/ContentView.swift
+++ test app/UsersModule/View/ContentView.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var viewModel = UsersViewModel()
+    @State private var hasError = false
+    @State private var error: UsersViewModel.UserError?
     
     var body: some View {
         NavigationView {
@@ -19,9 +21,20 @@ struct ContentView: View {
                     .navigationTitle("Users")
                 }
             }
-            .onAppear(perform: viewModel.fetchUsersCombine)
-            .alert(isPresented: $viewModel.hasError, error: viewModel.error) {
-                Button(action: viewModel.fetchUsersCombine) {
+//            .onAppear(perform: viewModel.fetchUsersCombine)
+            .task {
+                await execute()
+            }
+//            .alert(isPresented: $viewModel.hasError, error: viewModel.error)
+            .alert(isPresented: self.$hasError, error: self.error) {
+//                Button(action: viewModel.fetchUsersCombine) {
+//                    Text("Retry")
+//                }
+                Button {
+                    Task {
+                        await execute()
+                    }
+                } label: {
                     Text("Retry")
                 }
             }
@@ -32,5 +45,18 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+private extension ContentView {
+    func execute() async {
+        do {
+            try await viewModel.fetchUsersAsyncAwait()
+        } catch {
+            if let usrErr = error as? UsersViewModel.UserError {
+                self.hasError = true
+                self.error = usrErr
+            }
+        }
     }
 }
